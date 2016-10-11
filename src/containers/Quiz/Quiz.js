ce8d38b0bd19a1343c8d6000d5f033fab4f45446
Nick Dueber <nick.dueber@gmail.com>
2016-10-11 18:40:10 -0400

    adding rerender after actions

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { saveChanges, addQuestion, deleteQuestion } from '../../redux/modules/quizData.js';// import action creators
+import { saveQuestion, addQuestion, deleteQuestion, editQuestion } from '../../redux/modules/quiz.js';// import action creators
 
-const { func } = React.PropTypes;
+const { array, func } = React.PropTypes;
 const baseClass = 'Quiz';
-const BOARD_SIZE = 10;
 
 function mapStateToProps(state) {
   const { quiz } = state;
@@ -16,19 +15,99 @@ function mapStateToProps(state) {
 
 class Quiz extends React.Component {
   static propTypes = {
-    saveChanges: func.isRequired,
+    questions: array.isRequired,
+    saveQuestion: func.isRequired,
     addQuestion: func.isRequired,
     deleteQuestion: func.isRequired,
+    editQuestion: func.isRequired,
   }
 
   constructor(props) {
     super(props);
+    this.addQuestionHandler = this.addQuestionHandler.bind(this);
+    this.state = { key: Math.random() };
+  }
+
+  onEditInput(index) {
+    this.props.editQuestion(index);
+    this.setState({ key: Math.random() });
+  }
+
+  getQuestionClass(index) {
+    if (this.props.questions[index].edited === true) {
+      return baseClass + '__edited';
+    }
+    return baseClass + '__unedited';
+  }
+
+  addQuestionHandler() {
+    const questionInputVal = document.getElementById(baseClass + '__question').value;
+    const answerInputVal = document.getElementById(baseClass + '__answer').value;
+    this.props.addQuestion(questionInputVal, answerInputVal);
+    this.setState({ key: Math.random() });
+  }
+
+  saveQuestionHandler(index) {
+    const questionInputVal = document.getElementById(baseClass + '__question--' + index).value;
+    const answerInputVal = document.getElementById(baseClass + '__answer--' + index).value;
+    this.props.saveQuestion(questionInputVal, answerInputVal, index);
+    this.setState({ key: Math.random() });
+  }
+
+  deleteQuestionHandler(index) {
+    this.props.deleteQuestion(index);
+    this.setState({ key: Math.random() });
+  }
+
+  renderQuestions() {
+    const questions = this.props.questions.map((questionObj, index) => {
+      return (
+        <div key={index} className={`${baseClass}__question`}>
+          <input
+            id={`${baseClass}__question--${index}`}
+            className={`${baseClass}__input ${this.getQuestionClass(index)}`}
+            type="text"
+            name="question"
+            defaultValue={questionObj.question}
+            onChange={this.onEditInput.bind(this, index)}
+          />
+          <input
+            id={`${baseClass}__answer--${index}`}
+            className={`${baseClass}__input ${this.getQuestionClass(index)}`}
+            type="text"
+            name="answer"
+            defaultValue={questionObj.answer}
+            onChange={this.onEditInput.bind(this, index)}
+           />
+          <a className={`button`} onClick={this.saveQuestionHandler.bind(this, index)}>
+            Save Question
+          </a>
+          <a className={`button`} onClick={this.deleteQuestionHandler.bind(this, index)}>
+            Delete Question
+          </a>
+        </div>
+      );
+    });
+    return questions;
+  }
+
+  renderAddQuestion() {
+    return (
+      <div className={`${baseClass}__addQuestion`}>
+        <input id={`${baseClass}__question`} className={`${baseClass}__input`} type="text" name="question" defaultValue="Add Question Here"/>
+        <input id={`${baseClass}__answer`} className={`${baseClass}__input`} type="text" name="answer" defaultValue="Add Answer Here"/>
+        <a className={`button`} onClick={this.addQuestionHandler}>
+          add question
+        </a>
+      </div>
+    );
   }
 
   render() {
     return (
-      <div className={`${baseClass}`}>
-        Hello World
+      <div key={this.state.key} className={`${baseClass}`}>
+        {this.renderQuestions()}
+        {this.renderAddQuestion()}
       </div>
     );
   }
@@ -36,7 +115,5 @@ class Quiz extends React.Component {
 
 
 export default connect(mapStateToProps,
-  (dispatch) => bindActionCreators({ saveChanges, addQuestion, deleteQuestion }, dispatch)
+  (dispatch) => bindActionCreators({ saveQuestion, addQuestion, deleteQuestion, editQuestion }, dispatch)
 )(Quiz);
-
-//  (dispatch) => bindActionCreators({ saveChanges, addQuestion, deleteQuestion }, dispatch)
--- src/redux/modules/quiz.js
+++ src/redux/modules/quiz.js
@@ -2,32 +2,45 @@
 const SAVE_CHANGES = 'SAVE_CHANGES';
 const ADD_QUESTION = 'ADD_QUESTION';
 const DELETE_QUESTION = 'DELETE_QUESTION';
-
-// function variable constants
-const BOARD_SIZE = 10;
-const LEFT = 'left';
-const RIGHT = 'right';
-const UP = 'up';
-const DOWN = 'down';
-const DIRECTIONS_TO_PLACE_BOATS = [LEFT, RIGHT, UP, DOWN];
+const EDIT_QUESTION = 'EDIT_QUESTION';
 
 const initialState = {
-  questions: null,
+  questions: [],
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case SAVE_CHANGES:
+      const questionObj = {'question': action.question, 'answer': action.answer, 'editting': false};
+      let questions = JSON.parse(JSON.stringify(state.questions));
+      questions[action.questionId] = questionObj;
       return {
         ...state,
       };
     case ADD_QUESTION:
+      const question = {'question': action.question, 'answer': action.answer, 'editting': false};
+      let questions = JSON.parse(JSON.stringify(state.questions));
+      questions.push(question);
       return {
         ...state,
+        questions
       };
     case DELETE_QUESTION:
+      let questions = JSON.parse(JSON.stringify(state.questions));
+      console.log('in delete questions');
+      console.log(questions);
+      questions.splice(action.questionId, 1);
+      console.log(questions);
       return {
         ...state,
+        questions
+      };
+    case EDIT_QUESTION:
+      let questions = JSON.parse(JSON.stringify(state.questions));
+      questions[action.questionId].editting = true;
+      return {
+        ...state,
+        questions
       };
     default:
       return state;
@@ -42,24 +55,33 @@ export default function reducer(state = initialState, action = {}) {
 /**
  * Called on input change
  */
-export function saveChanges(questions) {
+export function saveQuestion(question, answer, questionId) {
   // add call to server here
   return {
     type: SAVE_CHANGES,
-    questions: questions
+    question: question,
+    answer: answer,
+    questionId: questionId
   };
 }
 
-export function addQuestion(question) {
+export function editQuestion(questionId) {
+  return {
+    type: EDIT_QUESTION,
+    questionId: questionId
+  };
+}
+
+export function addQuestion(question, answer) {
   // add call to server here
   return {
     type: ADD_QUESTION,
-    question: question
+    question: question,
+    answer: answer,
   };
 }
 
 export function deleteQuestion(questionId) {
-  // let shipsOnBoard = JSON.parse(JSON.stringify(ships));
   // add call to server here
   return {
     type: DELETE_QUESTION,
